3ca3afd3974b7aeaed74b676305f598a59c32bd1
nxiiio <w.orellana@duocuc.cl>
2026-06-14 20:52:14 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const {
-  DB_HOST = "127.0.0.1"
+  DB_HOST = "127.0.0.1",
   DB_USER = "root",
   DB_PASSWORD = "admin123",
   DB_NAME = "tienda_perritos",
